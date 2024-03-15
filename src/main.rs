5205c119d82f326fda3a771fc1c165684325f55b
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2024-03-15 16:01:01 -0500

    Load assets at startup, display blue pointer

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
-use bevy::prelude::*;
-use game::TurnBasedGameplayPlugin;
+use bevy::{prelude::*, log::LogPlugin};
 use ui::ConnectFourUIPlugin;
 
 mod board;
-mod game;
+// mod game;
 mod ui;
+
+#[derive(States, Debug, Clone, Eq, PartialEq, Hash)]
+pub enum AppState {
+    MainMenu,
+    Loading,
+    TakingTurn,
+    EvaluatingEndConditions,
+    EndConditionMet,
+}
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(TurnBasedGameplayPlugin)
+        .insert_state(AppState::MainMenu)
+
+        .add_plugins(DefaultPlugins.set(
+            LogPlugin {
+                filter: "info,wgpu_core=warn,wgpu_hal=warn,connect_four=debug".into(),
+                level: bevy::log::Level::DEBUG,
+                update_subscriber: None,
+            }
+        ))
         .add_plugins(ConnectFourUIPlugin)
+
         .run();
 }
+
 
--- src/ui.rs
+++ src/ui.rs
@@ -6,6 +6,44 @@ use bevy::prelude::*;
 pub struct ConnectFourUIPlugin;
 
 impl Plugin for ConnectFourUIPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup_ui);
+    }
 }
+
+#[derive(Resource, Debug, Clone, Eq, PartialEq, Hash)]
+pub struct AssetHandles {
+    pub tile_grey: Handle<Image>,
+
+    pub token_red: Handle<Image>,
+    pub pointer_red: Handle<Image>,
+
+    pub token_blue: Handle<Image>,
+    pub pointer_blue: Handle<Image>,
+}
+
+fn setup_ui(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    debug!("Setting up UI");
+    let asset_handles = AssetHandles {
+        tile_grey: asset_server.load("tile_grey.png"),
+
+        token_red: asset_server.load("red_body_rhombus.png"),
+        pointer_red: asset_server.load("red_hand_point.png"),
+
+        token_blue: asset_server.load("blue_body_squircle.png"),
+        pointer_blue: asset_server.load("blue_hand_point.png"),
+    };
+
+    commands.insert_resource(asset_handles.clone());
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_handles.pointer_blue.clone(),
+        ..Default::default()
+    });
+    debug!("UI setup complete");
+}
+
 
